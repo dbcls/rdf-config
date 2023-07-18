fbc276b747055d2e455edecd813c4135dba5c874
Tatsuya Nishizawa <nishizawa.tatsuya@gmail.com>
2023-07-18 13:08:45 +0900

    Uniprot sparql (#68)
    * Fixed SPARQL generation
    
    * Fixed to work with Ruby 2.6.

--- lib/rdf-config/sparql/select_generator.rb
+++ lib/rdf-config/sparql/select_generator.rb
@@ -10,29 +10,9 @@ class RDFConfig
           [
             'SELECT',
             distinct? ? 'DISTINCT' : '',
-            valid_variables
+            select_variables(add_question_mark: true)
           ].flatten.reject { |term| term.to_s.empty? }.join(' ')
         ]
-      end
-
-      def valid_variables
-        if join?
-          valid_variables_by_query
-        else
-          variables_handler.variables_for_select.map { |name| variable_name_for_sparql(name, true) }
-        end
-      end
-
-      def valid_variables_by_query
-        variable_names = []
-        @opts[:query].each do |query|
-          config_name, variable_name = query.split(':')
-          @config = @configs.select { |config| config.name == config_name }.first
-          name = valid_variable(variable_name)
-          variable_names << variable_name_for_sparql(name, true) unless name.nil?
-        end
-
-        variable_names.uniq
       end
     end
   end
